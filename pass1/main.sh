43de287b168de72ce0cd5facba82e8d06982a8a9
Gabriel Guerra <gfranklings@gmail.com>
2023-06-02 19:01:41 -0400

    [chore] typo

--- pass1/main.sh
+++ pass1/main.sh
@@ -2,13 +2,13 @@ right_pth() {
     cd ${@}/sources/builders/pass1
 }
 
-right_pth
+right_pth ${@}
 
 . configure.sh ${@}
 
 for item in $(ls ./); do
     if [[ "$item" != "configure.sh" && "$item" != "main.sh" ]]; then
-        right_pth
+        right_pth ${@}
         . $item ${@}
     fi
 done
